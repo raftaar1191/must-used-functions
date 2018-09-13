27adffcaac75cc979f3a77a49c16e823054a0c71
raftaar1191 <raftaar1191@gmail.com>
2018-09-13 17:03:26 +0530

    rename plugin

--- give-mu-plugin.php
+++ give-mu-plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Give - Must Used Plugin
+ * Plugin Name: Must Used Plugin
  * Plugin URI: https://raftaar1191.com
- * Description: Must used functions required when working with Give Plugin.
+ * Description: Must used functions required when developing sites.
  * Author: raftaar1191
  * Author URI: https://raftaar1191.com
  * Version: 1.0
@@ -20,8 +20,15 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @return int
  */
-function give_mu_option_show_avatars() {
+function raf_mu_option_show_avatars() {
 	return 0;
 }
 
-add_filter( 'option_show_avatars', 'give_mu_option_show_avatars', 10, 0 );
+add_filter( 'option_show_avatars', 'raf_mu_option_show_avatars', 10, 0 );
+
+/**
+ * Print out put in new debug log file
+ */
+function raf_mu_print_error( $output, $filename = 'debug_new.log' ) {
+	error_log( print_r( $output, true ) . "\n", 3, WP_CONTENT_DIR . '/' . $filename );
+}
